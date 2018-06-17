1247845406160ce98d9977c0d5f0aab4f5b32840
Wissem <wissemkhaled69@gmail.com>
2018-06-17 20:56:39 +0200

    Add icon on my skill and adjust some LESS

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
+
+// Navigation Barre
 $(document).ready(function () {
     $('.menu-button>a').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
-      console.log('click');
         let target = this.hash,
         $target = $(target);
         $('html, body').stop().animate({
@@ -21,3 +22,41 @@ function onScroll(event){
         let refElement = $(currLink.attr("href"));
     });
 }
+
+//KONAMI CODE
+var k = [38, 38, 40, 40, 37, 39, 37, 39,66,65],
+n = 0;
+$(document).keydown(function (e) {
+    if (e.keyCode === k[n++]) {
+        if (n === k.length) {
+           console.log('Konami code validé =D');
+            alert('Hey ! On fait une ranked ? :D');
+            n = 0;
+            return false;
+        }
+    }
+    else {
+        n = 0;
+    }
+});
+
+
+$(document).ready(function () {
+    $('.menu-button>a').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        let target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 1500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+$(document).on("click", ".footer-button>li>a", function () {
+    alert('Mon adresse Email : wissemkhaled69@gmail.com (Le temps que j\'implémente un formulaire)');
+});
+ 
